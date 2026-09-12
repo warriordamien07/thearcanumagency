459ed476ec9bd38508aebd76d8c20d10b722eae9
Anonymous! <115762790+warriordamien07@users.noreply.github.com>
2026-09-13 00:33:48 +0100

    Add files via upload

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -2,30 +2,16 @@
 // of wherever it is placed (ink on light, paper on dark).
 export function ArrowIcon() {
   return (
-    <span className="arrow-icon" aria-hidden="true" style={{ display: "inline-flex", overflow: "hidden" }}>
-      <svg viewBox="0 0 340.034 340.034" fill="currentColor" aria-hidden="true" focusable="false" style={{ width: "1em", height: "1em", display: "block" }}>
-        <polygon points="222.814,52.783 200.902,74.686 280.748,154.528 0,154.528 0,185.513 280.748,185.513 200.902,265.353 222.814,287.252 340.034,170.023" />
-      </svg>
-    </span>
+    <svg viewBox="0 0 340.034 340.034" fill="currentColor" aria-hidden="true" focusable="false">
+      <polygon points="222.814,52.783 200.902,74.686 280.748,154.528 0,154.528 0,185.513 280.748,185.513 200.902,265.353 222.814,287.252 340.034,170.023" />
+    </svg>
   );
 }
 
-export function DotsIcon() {
+export function ChevronDownIcon() {
   return (
-    <svg viewBox="0 0 342.382 342.382" fill="currentColor" aria-hidden="true" focusable="false">
-      <g>
-        <g>
-          <g>
-            <path d="M45.225,125.972C20.284,125.972,0,146.256,0,171.191c0,24.94,20.284,45.219,45.225,45.219 c24.926,0,45.219-20.278,45.219-45.219C90.444,146.256,70.151,125.972,45.225,125.972z"></path>
-          </g>
-          <g>
-            <path d="M173.409,125.972c-24.938,0-45.225,20.284-45.225,45.219c0,24.94,20.287,45.219,45.225,45.219 c24.936,0,45.226-20.278,45.226-45.219C218.635,146.256,198.345,125.972,173.409,125.972z"></path>
-          </g>
-          <g>
-            <path d="M297.165,125.972c-24.932,0-45.222,20.284-45.222,45.219c0,24.94,20.29,45.219,45.222,45.219 c24.926,0,45.217-20.278,45.217-45.219C342.382,146.256,322.091,125.972,297.165,125.972z"></path>
-          </g>
-        </g>
-      </g>
+    <svg viewBox="0 0 32 32" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true" focusable="false">
+      <path d="M8 11l8 8 8-8" />
     </svg>
   );
 }
